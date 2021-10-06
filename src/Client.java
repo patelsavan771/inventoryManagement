c75aeb4d2c0298e0f5a327f662421622aaf6f226
Savan <patelsavan771@gmail.com>
2021-10-06 20:53:22 +0530

    Create Client.java

---
+++ src/Client.java
@@ -0,0 +1,6 @@
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("this is starting of client program of Inventory Management");
+    }
+}
